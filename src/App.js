8fac20969b0ceabc2b6fe3e113e3fbdd32fc0e6a
debasispanda <imdebasispanda@gmail.com>
2020-11-12 22:19:39 +0530

    feat: add ci with lint, test and build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/:page" component={Dashboard} />
-          <Route exact path="/">
-            <Redirect to="/0" />
-          </Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/:page" component={Dashboard} />
+        <Route exact path="/">
+          <Redirect to="/0" />
+        </Route>
+      </Switch>
     </div>
   );
 };
